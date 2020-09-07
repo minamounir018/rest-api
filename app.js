03499a071fd95ee6edf2929b8421af6b40285243
Mina <minamoner01811@gmail.com>
2020-09-07 20:32:32 +0200

    secound

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use('/newproducts', NewProuduct);
 app.use('/contactus', ContactUs);
 
 app.use((req, res, next) => {
-    const error= new Error('Not found');
+    const error= new Error('Error not found');
     error.status=404;
     next(error);
 });
