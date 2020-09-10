0a7581d101ddc1fe0090a2dec8d5b451e99e0ee9
Mina <minamoner01811@gmail.com>
2020-09-10 15:51:43 +0200

    add

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app =express();
+const app = express();
 
 const morgan = require('morgan');
 
@@ -45,7 +45,11 @@ app.use('/products', productRoutes);
 app.use('/newproducts', NewProduct);
 app.use('/contactus', ContactUs);
 
-app.use(NewProduct);
+app.use((req, res, next) => {
+    const error= new Error('Error not found');
+    error.status=404;
+    next(error);
+});
 
 app.use((error, req, res, next) => {
     res.status(error.status || 500);
