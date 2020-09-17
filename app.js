049db20ea13315c0742085d1b7b13fe1d32782da
Mina <minamoner01811@gmail.com>
2020-09-17 08:54:09 +0200

    edit

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const app = express();
 
@@ -65,76 +64,5 @@ app.use((error, req, res, next) => {
             message: error.message
         }
     });
-});
-
-=======
-const express = require('express');
-const app = express();
-
-const morgan = require('morgan');
-
-const bodyParser = require('body-parser');
-
-const mongoose = require('mongoose');
-
-const productRoutes =require('./api/routes/products');
-const OrdersRoutes =require('./api/routes/Orders');
-const NewProduct =require('./api/routes/NewProducts');
-const DecorProduct =require('./api/routes/DecorProducts');
-const BathProduct =require('./api/routes/BathProducts');
-const GiftProduct =require('./api/routes/GiftProducts');
-const ContactUs =require('./api/routes/ContactUs');
-
-mongoose.connect(
-    'mongodb+srv://Mina-Mounir:minamoneR018@mina-rest-api.qyyly.mongodb.net/Mina-rest-api?retryWrites=true&w=majority',
-{ 
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-mongoose.Promise = global.Promise;
-
-app.use(morgan('dev'));
-app.use('/uploads', express.static('uploads'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Headers', 
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-        ); 
-        if(req.method === 'OPTIONS')
-        {
-            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE,GET');
-            return res.status(200).json({});
-        }
-        next();
-
-});
-
-app.use('/orders', OrdersRoutes);
-app.use('/products', productRoutes);
-app.use('/newproducts', NewProduct);
-app.use('/decorproducts', DecorProduct);
-app.use('/bathproducts', BathProduct);
-app.use('/giftproducts', GiftProduct);
-app.use('/contactus', ContactUs);
-
-app.use((req, res, next) => {
-    const error= new Error('Error not found');
-    error.status=404;
-    next(error);
-});
-
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error:{
-            message: error.message
-        }
-    });
-});
-
->>>>>>> aa414de351fe56e57f4ff686b530aac6e9ab26fb
+})
 module.exports = app;
